5681394396f6e793a72d970d7da8a93c762f88c8
Vivian Lo <yunyubee@gmail.com>
2019-10-28 16:36:37 +0800

    install express-session and connect-flash. Confirm user password and unique email

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const app = express()
 const handlebars = require('express-handlebars')
 const db = require('./models') //include DB
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const port = 3000
 
 //view engine
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 //include routes
 require('./routes')(app)
