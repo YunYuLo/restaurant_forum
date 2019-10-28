37f10bdae56d080208a6fb600e9360734f473c56
Vivian Lo <yunyubee@gmail.com>
2019-10-28 16:04:54 +0800

    Install bcrypt-nodejs and body-parser. Add userController and signup page

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const db = require('./models') //include DB
+const bodyParser = require('body-parser')
 const port = 3000
 
 //view engine
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //include routes
 require('./routes')(app)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 
 module.exports = app => {
   //index page
@@ -17,5 +18,9 @@ module.exports = app => {
   app.get('/admin', (req, res) => res.render('/admin/restaurants'))
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+  //user sing up
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 
 }
