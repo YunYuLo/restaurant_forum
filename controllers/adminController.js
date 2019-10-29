de93390007f3ffef6e4457e1de4b61ce81549223
Vivian Lo <yunyubee@gmail.com>
2019-10-29 14:26:22 +0800

    Update admin create page for editing usage. Add admin restaurant put feature.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -31,6 +31,31 @@ const adminController = {
     return Restaurant.findByPk(req.params.id).then(restaurant => {
       return res.render('admin/restaurant', { restaurant })
     })
+  },
+  editRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id).then(restaurant => {
+      return res.render('admin/create', { restaurant })
+    })
+  },
+  putRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "Name didn't exist.")
+      return res.redirect('back')
+    }
+    return Restaurant.findByPk(req.params.id)
+      .then((restaurant) => {
+        restaurant.update({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description
+        })
+          .then((restaurant) => {
+            req.flash('success_messages', "Restaurant was successfully to update.")
+            res.redirect('/admin/restaurants')
+          })
+      })
   }
 
 }
