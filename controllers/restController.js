dbb74136a479966504446ab485114384168854ae
Vivian Lo <yunyubee@gmail.com>
2019-11-04 20:32:46 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,19 +2,32 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 let restController = {
   //action: 瀏覽餐廳頁面
   getRestaurants: (req, res) => {
+    let offset = 0
     let whereQuery = {}
     let categoryId = ''
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery['CategoryId'] = categoryId
     }
-    Restaurant.findAll({ include: Category, where: whereQuery })
-      .then((restaurants) => {
-        const data = restaurants.map(r => ({
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
+    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit })
+      .then(result => {
+        //data for pagination
+        let page = Number(req.query.page) || 1
+        let pages = Math.ceil(result.count / pageLimit)
+        let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+        let prev = page - 1 < 1 ? 1 : page - 1
+        let next = page + 1 > pages ? pages : page + 1
+        //clean up restaurant data
+        const data = result.rows.map(r => ({
           ...r.dataValues,
           description: r.dataValues.description.substring(0, 50)
         }))
@@ -22,8 +35,12 @@ let restController = {
           .then((categories) => {
             return res.render('restaurants', {
               restaurants: data,
-              categories: categories,
-              categoryId: categoryId
+              categories,
+              categoryId,
+              page,
+              totalPage,
+              prev,
+              next
             })
           })
       })
