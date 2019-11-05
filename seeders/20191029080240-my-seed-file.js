84a7781069a684ccd7fae5e6e6b80a79bc110806
Vivian Lo <yunyubee@gmail.com>
2019-11-05 13:08:37 +0800

    Add image column to Users and update seeder

--- seeders/20191029080240-my-seed-file.js
+++ seeders/20191029080240-my-seed-file.js
@@ -20,6 +20,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
       name: 'root',
+      image: faker.image.imageUrl(),
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -27,6 +28,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user1',
+      image: faker.image.imageUrl(),
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -34,6 +36,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user2',
+      image: faker.image.imageUrl(),
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
