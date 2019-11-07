2b61de1849bcc9c9a7804dbb946e144d21b52518
Vivian Lo <yunyubee@gmail.com>
2019-11-07 11:25:56 +0800

    Add addFollowing and removeFollowing to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const db = require('../models')
 const User = db.User
 const Favorite = db.Favorite
 const Like = db.Like
+const Followship = db.Followship
 
 let userController = {
   signUpPage: (req, res) => {
@@ -102,6 +103,27 @@ let userController = {
       }))
       users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
       return res.render('topUser', { users })
+    })
+  },
+  addFollowing: (req, res) => {
+    return Followship.create({
+      followerId: req.user.id,
+      followingId: req.params.userId
+    }).then((followship) => {
+      return res.redirect('back')
+    })
+  },
+  removeFollowing: (req, res) => {
+    return Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId
+      }
+    }).then((followship) => {
+      followship.destroy()
+        .then((followship) => {
+          return res.redirect('back')
+        })
     })
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -80,4 +80,8 @@ module.exports = (app, passport) => {
   app.post('/like/:restaurantId', authenticated, userController.addLike)
   app.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
+  //followship
+  app.post('/following/:userId', authenticated, userController.addFollowing)
+  app.delete('/following/:userId', authenticated, userController.removeFollowing)
+
 }
