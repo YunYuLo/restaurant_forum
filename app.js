02ac2fffcd1af2660ed98e1216c1f87aad334898
Vivian Lo <yunyubee@gmail.com>
2019-11-11 23:44:47 +0800

    Refactor adminController.postRestaurant

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.engine('handlebars', handlebars({
 }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(methodOverride('_method'))
 app.use(session({
   secret: "secret",
--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -15,11 +15,16 @@ const adminController = {
       return res.json(data)
     })
   },
+  postRestaurant: (req, res) => {
+    adminService.postRestaurant(req, res, (data) => {
+      return res.json(data)
+    })
+  },
   deleteRestaurant: (req, res) => {
     adminService.deleteRestaurant(req, res, (data) => {
       return res.json(data)
     })
-  }
+  },
 }
 
 module.exports = adminController
